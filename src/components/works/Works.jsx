5788acbbd2ae8626d2d1cd8d4333c7145ab9210a
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-11-10 09:12:42 +0100

    did some styling

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,22 @@ import "./works.scss"
 
 export default function Works() {
   return (
-    <div className="works" >
+    <div className="works" id="works">
+      <h1>[ 01 ]</h1>
+      <div className="photo0">
+        <img src="assets/00.png" alt="k"></img>
+      </div>
+      <h2>[ 02 ]</h2>
+      <div className="photo09">
+        <img src="assets/0.png" alt="k"></img>
+        <img src="assets/77.png" alt="k" id="pp"></img>
+      </div>
+      <h3>[ 03 ]</h3>
+      <div className="photo010">
+        <img src="assets/33.png" alt="k"></img>
+        <img src="assets/35.png" alt="k" id="po"></img>
+      </div>
+
 
     </div>
   )
