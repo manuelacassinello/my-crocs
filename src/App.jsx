365d0085698e798f88355cc1a07454549051e661
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-11-22 12:42:33 +0100

    did some

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Intro from "./components/intro/Intro";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
 import "./app.scss";
-import Cursor from "./Cursor";
+
 
 
 
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="app">
       <Topbar />
-      <Cursor />
++
       <div className="sections">
 
         <Intro />
--- src/Link.js
+++
@@ -1,3 +0,0 @@
-const Link = () => {
-  return <img
-}
